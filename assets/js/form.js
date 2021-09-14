d0ad2ed3d4854c6dc724e520aa653cb333f10846
Timo Klock <tmklock@googlemail.com>
2021-09-15 00:06:21 +0200

    small adjustments to form and js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-    document.getElementById("form-send-response").style.display = "none";
-    document.getElementById("form-send-response-negative").style.display = "none";
-});
-
-
 var form = document.getElementById("project_form");
 async function handleSubmit(event) {
   event.preventDefault();
